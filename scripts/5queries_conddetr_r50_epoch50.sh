3a04c81c42f91d527d179c92906f38c1e3a988c8
yweizhang <zhang2486@wisc.edu>
2022-06-08 21:29:47 -0500

    debugging

--- scripts/5queries_conddetr_r50_epoch50.sh
+++ scripts/5queries_conddetr_r50_epoch50.sh
@@ -2,8 +2,8 @@ script_name1=`basename $0`
 script_name=${script_name1:0:${#script_name1}-3}
 
 python -m torch.distributed.launch \
-    --nproc_per_node=8 \
+    --nproc_per_node=4 \
     --use_env \
-    main.py  --batch_size 4 --num_queries 5\
-    --coco_path ../data/coco \
+    main.py --batch_size 4 --num_queries 5\
+    --coco_path ../coco \
     --output_dir output/$script_name
