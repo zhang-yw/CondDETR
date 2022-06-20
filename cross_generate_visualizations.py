e6bf1098a12534c648abfa64f7a92e090a84641d
yweizhang <zhang2486@wisc.edu>
2022-06-20 13:26:59 -0500

    fix

--- cross_generate_visualizations.py
+++ cross_generate_visualizations.py
@@ -167,6 +167,7 @@ for fname in filenames:
         # print(final_dec_attn_weights.shape)
         # print(dec_attn_weights.shape)
         print(final_dec_attn_weights_idx)
+        exit(0)
         show = final_dec_attn_weights_idx[0]*dec_attn_weights[0]
         # print(show.shape)
         for i in range(4):
